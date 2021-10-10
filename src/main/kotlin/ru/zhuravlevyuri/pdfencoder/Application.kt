555c241578a935147f1a1375be3b8a9d9b9af391
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-10-10 13:11:03 +0300

    change to cio

--- src/main/kotlin/ru/zhuravlevyuri/pdfencoder/Application.kt
+++ src/main/kotlin/ru/zhuravlevyuri/pdfencoder/Application.kt
@@ -4,12 +4,12 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
-            call.respondText("Hello, world!")
+            call.respondText("<h1>PDF encoder</h1>\nHello, world!\nDEBUG=${testing}")
         }
     }
 }
